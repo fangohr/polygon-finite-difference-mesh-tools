956e12a0e11e54d282788da6dbb57afd7b4d64b0
Hans Fangohr [iota] <fangohr@soton.ac.uk>
2015-09-09 16:06:27 +0100

    Fake pep8 test by calling external pep8 tool.
    Can probably be done more elegantly.

---
+++ polygonmeshtools/test_pep8.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def test_pep8():
+    """Runs pep8 over all code"""
+    path = '.'
+
+    command = "pep8 {}".format(path)
+    try:
+        subprocess.check_output(command, shell=True)
+    except subprocess.CalledProcessError as ex:
+        # caught error.
+        print("Output with error:\n")
+        for line in ex.output.splitlines():
+            print(line)
+        # some PEP8 problem
+        assert False
+
+if __name__ == "__main__":
+    test_pep8()
