ef38e6ee146687298846c7867e3dea26f455249b
Xander Marjoram <am31g12@soton.ac.uk>
2015-09-10 10:02:55 +0100

    Cleaning up code, setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 import os
-from distutils.core import setup
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name = "polygonmeshtools",
-    packages=["polygonmeshtools"],
+    packages = ["polygonmeshtools"],
     version = "0.3",
-    description = "A package for creating polygon-shaped geometries from cuboidal finite difference elements",
+    description = "A package for creating polygon-shaped geometries \
+        from cuboidal finite difference elements",
     author = "Xander Marjoram",
     author_email = "am31g12@soton.ac.uk",
-    keywords = ["polygon", "mesh", "geometry", "finite", "difference", "fidimag"],
+    keywords = [
+            "polygon", "mesh", "geometry",
+            "finite", "difference", "fidimag"
+        ],
     url = "https://bitbucket.org/fangohr/polygon-finite-difference-mesh-tools/get/tip.tar.gz",
     #classifiers = [],
 )
