b3e3dfc07023e4e32e2c6040832ceb9665f00029
Xander Marjoram <am31g12@soton.ac.uk>
2015-09-10 10:44:51 +0100

    Tidying up code, plot.py.

--- polygonmeshtools/plot.py
+++ polygonmeshtools/plot.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
 
-def plot_circular_fidi_mesh(diameter, x_spacing=2, y_spacing=2,
-                            centre_mesh='auto', show_axes=True, show_title=True):
+def plot_circular_fidi_mesh(
+    diameter, x_spacing=2, y_spacing=2, centre_mesh='auto', 
+    show_axes=True, show_title=True 
+    ):
     """
     Plots a representation of a circular mesh of specified diameter
     comprised of rectangular elements of size x_spacing x y_spacing nm.
@@ -17,20 +19,29 @@ def plot_circular_fidi_mesh(diameter, x_spacing=2, y_spacing=2,
 
     Miscellaneous info:
     ~~~~~~~~~~~~~~~~~~~
-
-    centre_mesh = True    This will set the middle of the mesh to lie at the centre of an element.
-    centre_mesh = False     "    "   "   "     "    "  "    "   "  "   "  "  corner  "  "    "   .
-    centre_mesh = 'auto'  The centering will be decided based on whichever will fit the geometry best.
+    This will set the middle of the mesh to lie at the centre of an 
+    element.
+    centre_mesh = True
+    
+    This will set the middle of the mesh to lie at the corner of an 
+    element.
+    centre_mesh = False
+    
+    The centering will be decided based on whichever will fit the
+    geometry best.
+    centre_mesh = 'auto'
 
     Known bugs:
     ~~~~~~~~~~~
 
-    When the diameter/x_spacing or y_spacing does not produce an integer, the mesh will not be centered correctly.
+    When the x_spacing OR y_spacing is not a factor of the diameter, 
+    the mesh will not be centered correctly.
+    
     It is recommended that centre_mesh be set to True in this case.
     """
 
     #initial setup of figure
-    fig = plt.figure(figsize=(diameter**0.45, diameter**0.45))
+    fig = plt.figure(figsize = (diameter**0.45, diameter**0.45))
     ax0 = fig.add_subplot(111)
 
     ax0.set_xlim(-0.5*diameter, 0.5*diameter)
@@ -58,25 +69,42 @@ def plot_circular_fidi_mesh(diameter, x_spacing=2, y_spacing=2,
     unit_area = x_spacing*y_spacing
 
     if centre_mesh is True:
-        #produces lists of the form [0,2,-2,4,-4,6,-6,8,-8,10,-10]
-        #(spacing = 2; diameter = 20, in this example)
-        #trailing slice index is to remove leading zero which would cause duplicate patches
-        xlist = sorted(list(range(0, (diameter/2)+1, x_spacing))
-                       + list(range(0, -(diameter/2)-1, -x_spacing)),
-                       key=abs)[1:]
-        ylist = sorted(list(range(0, (diameter/2)+1, y_spacing))
-                       + list(range(0, -(diameter/2)-1, -y_spacing)),
-                       key=abs)[1:]
+        #produce lists of the form [0,2,-2,4,-4,6,-6,8,-8,10,-10]
+        # (spacing = 2; diameter = 20, in this^ example)
+        
+        #the idea is to start in the centre and then work outwards,
+        # alternating sides
+        
+        #[1:] removes first zero which would cause duplicate patches
+        xlist = sorted(
+            list(range(0, (diameter/2)+1, x_spacing)) +
+            list(range(0, -(diameter/2)-1, -x_spacing)), key=abs
+            )[1:]
+        ylist = sorted(
+            list(range(0, (diameter/2)+1, y_spacing)) + 
+            list(range(0, -(diameter/2)-1, -y_spacing)), key=abs
+            )[1:]
 
         for y in ylist:
             for x in xlist:
                 box_coord = (x - 0.5*x_spacing, y - 0.5*y_spacing)
                 if CartesianCoords(x,y).r <= 0.5*diameter:
                     #plot rectangle
-                    ax0.add_patch(patches.Rectangle(box_coord, x_spacing, y_spacing, alpha = 0.25))
+                    ax0.add_patch(
+                        patches.Rectangle(
+                            box_coord, x_spacing, y_spacing, alpha = 0.25
+                            )
+                        )
                     area += unit_area
                 else:
-                    break #if circle boundary exceeded once, no point proceeding with this row
+                    #if circle boundary is exceeded once, there is no
+                    # point proceeding with this row, as all following
+                    # coordinates will also lie outside the circle
+                    
+                    #(recall: we are working outwards and circles
+                    # are symmetric)
+                    break
+                    
     elif centre_mesh == False:
         xmax = int((diameter/2.)+(diameter%x_spacing))
         ymax = int((diameter/2.)+(diameter%y_spacing))
@@ -85,30 +113,46 @@ def plot_circular_fidi_mesh(diameter, x_spacing=2, y_spacing=2,
         for y in ylist:
             for x in xlist:
                 centre_coord = (x + 0.5*x_spacing, y + 0.5*y_spacing)
-                if (CartesianCoords(centre_coord[0], centre_coord[1]).r <= 0.5*diameter):
+                if (CartesianCoords(centre_coord[0], centre_coord[1]).r <= 
+                        0.5*diameter):
                     #plot element
-                    ax0.add_patch(patches.Rectangle((x,y), x_spacing, y_spacing, alpha = 0.25))
+                    ax0.add_patch(
+                        patches.Rectangle(
+                            (x,y), x_spacing, y_spacing, alpha = 0.25
+                            )
+                        )
+                        
                     area += unit_area
                 else:
                     pass 
     else:
-        raise ValueError("Unrecognised value for centre_mesh! Please choose True, False or 'auto'.")
+        raise ValueError(
+            "Unrecognised value for centre_mesh! \
+            Please choose True, False or 'auto'."
+            )
     
     elem_count = area/(x_spacing*y_spacing)
     
     if show_title == True:
-        ax0.set_title("Finite difference mesh demonstration\n        Diameter = {} nm\n{} elements of size {}x{} nm"
-                      .format(diameter, elem_count, x_spacing, y_spacing))
+        ax0.set_title(
+            "Finite difference mesh demonstration\n\
+            Diameter = {} nm\n\
+            {} elements of size {}x{} nm".format(
+                diameter, elem_count, x_spacing, y_spacing
+                )
+            )
     #return area
 
 
 
-def plot_poly_fidi_mesh(diameter,n,x_spacing=2,y_spacing=2,rotation=0,translate=(0,0),
-                        centre_mesh='auto',show_axes=True, show_title=True):
+def plot_poly_fidi_mesh(
+    diameter, n, x_spacing=2, y_spacing=2, rotation=0, translate=(0,0),
+    centre_mesh='auto', show_axes=True, show_title=True
+    ):
     """
-    Plots a representation of an n-sided polygon mesh with a containing circle
-    of specified diameter, comprised of rectangular elements of size 
-    x_spacing*y_spacing (in nanometres).
+    Plots a representation of an n-sided polygon mesh with a containing
+    circle of specified diameter, comprised of rectangular elements of
+    size x_spacing*y_spacing (in nanometres).
     
     
     Requires the following imports:
@@ -122,19 +166,24 @@ def plot_poly_fidi_mesh(diameter,n,x_spacing=2,y_spacing=2,rotation=0,translate=
     Miscellaneous info:
     ~~~~~~~~~~~~~~~~~~~
     
-    centre_mesh = True    will set the middle of the mesh to lie at the centre of an element.
-    centre_mesh = False     "   "   "     "    "  "    "   "  "   "  "  corner  "  "    "   .
-    centre_mesh = 'auto'  will decide True or False based on whichever will best fit the geometry.
+    This will set the middle of the mesh to lie at the centre of an
+    element.
+    centre_mesh = True
     
-    The radius is defined as the circumradius, i.e., the distance from the circumcentre
-    of the shape to one of its vertices.
-    For example, to get a square with edge length 50, diameter must be given as 50*(2**0.5).
+    This will set the middle of the mesh to lie at the corner of an
+    element.
+    centre_mesh = False
     
+    This will decide True or False based on whichever will best fit the
+    geometry.
+    centre_mesh = 'auto'
     
-    Known bugs:
-    ~~~~~~~~~~~
-    Using find_circumradius to give exact side lengths can produce unusual
-    tails trailing off some corners.
+    The radius is defined as the circumradius, i.e., the distance from
+    the circumcentre of the shape to one of its vertices.
+    
+    For example, to get a square with an edge length of 50, the
+    diameter must be given as 50*(2**0.5), which is the distance from
+    the centre of the square to one of its corners.
     """
     
     #initial setup of figure
@@ -152,8 +201,8 @@ def plot_poly_fidi_mesh(diameter,n,x_spacing=2,y_spacing=2,rotation=0,translate=
         ax0.set_frame_on(False)    
     
     #decide if centering will prevent asymmetry
-    #maybe incorporate independent centering for x and y
-    #(tried this and it was extremely difficult...)
+    #in future, potentially decide x and y centring separately
+    # (will add a lot of lines of code and complexity, is it worth it?)
     if centre_mesh == 'auto':
         if diameter%(2*x_spacing) == 0 and diameter%(2*y_spacing) == 0:
             centre_mesh = False
@@ -164,57 +213,100 @@ def plot_poly_fidi_mesh(diameter,n,x_spacing=2,y_spacing=2,rotation=0,translate=
     unit_area = x_spacing*y_spacing
     
     if centre_mesh == True:
-        #produces lists of the form [0,2,-2,4,-4,6,-6,8,-8,10,-10]
-        #(spacing = 2; diameter = 20, in this example)
-        #trailing slice index is to remove leading zero which would cause duplicate patches
-        xlist = sorted(list(range(0, int(diameter/2)+1, x_spacing)) 
-                       + list(range(0,-int(diameter/2)-1,-x_spacing)), key=abs)[1:]
+        #produce lists of the form [0,2,-2,4,-4,6,-6,8,-8,10,-10]
+        # (spacing = 2; diameter = 20, in this^ example)
+        
+        #[1:] removes first zero which would cause duplicate patches
+        xlist = sorted(
+            list(range(0, int(diameter/2)+1, x_spacing)) + 
+            list(range(0,-int(diameter/2)-1,-x_spacing)), key=abs
+            )[1:]
         xlist = [x + translate[0] for x in xlist[:]]
-        ylist = sorted(list(range(0, int(diameter/2)+1, y_spacing)) 
-                      + list(range(0,-int(diameter/2)-1,-y_spacing)), key=abs)[1:]
+        
+        ylist = sorted(
+            list(range(0, int(diameter/2)+1, y_spacing)) + 
+            list(range(0,-int(diameter/2)-1,-y_spacing)), key=abs
+            )[1:]
         ylist = [y + translate[1] for y in ylist[:]]
     
         for y in ylist:
             for x in xlist:
                 box_coord = (x - 0.5*x_spacing, y - 0.5*y_spacing)
-                if in_poly(x, y, n=n, r=diameter/2.,
-                           rotation=rotation, translate=translate, plot=False):
+                if in_poly(
+                    x, y, n=n, r=diameter/2., rotation=rotation, 
+                    translate=translate, plot=False
+                    ):
                     #plot element
-                    ax0.add_patch(patches.Rectangle(box_coord, x_spacing, y_spacing, alpha = 0.25))
+                    ax0.add_patch(
+                        patches.Rectangle(
+                            box_coord, x_spacing, y_spacing, alpha = 0.25
+                            )
+                        )
+                        
                     area += unit_area
+                    
                 else:
                     pass
                 
     elif centre_mesh == False:
+    #start from top left corner and work across and down
+    # note: this is less efficient than centre_mesh = True, as the
+    # entire area is tested
         xmax = int((diameter/2.)+(diameter%x_spacing))
         ymax = int((diameter/2.)+(diameter%y_spacing))
+        
         xlist = [x + translate[0] for x in range(-xmax,xmax,x_spacing)]
         ylist = [y + translate[1] for y in range(-ymax,ymax,y_spacing)]
         
         for y in ylist:
             for x in xlist:
                 centre_coord = (x + 0.5*x_spacing, y + 0.5*y_spacing)
-                if in_poly(centre_coord[0], centre_coord[1], n=n, r=diameter/2.,
-                           rotation=rotation, translate=translate):
-                    ax0.add_patch(patches.Rectangle((x,y), x_spacing, y_spacing, alpha = 0.25))
+                
+                if in_poly(
+                    centre_coord[0], centre_coord[1], n=n, r=diameter/2., 
+                    rotation=rotation, translate=translate
+                    ):
+                    ax0.add_patch(
+                        patches.Rectangle(
+                            (x,y), x_spacing, y_spacing, alpha = 0.25
+                        )
+                    )
                     area += unit_area
+                    
                 else:
                     pass        
     else:
-        raise ValueError("Unrecognised value \"{}\" for centre_mesh! Please choose True, False or 'auto'."
-                         .format(centre_mesh))
+        raise ValueError(
+            "Unrecognised value \"{}\" for centre_mesh! \
+            Please choose True, False or \"auto\".".format(centre_mesh)
+            )
     
     #plot reference polygon
     coords = np.empty((n,2))
     for i in range(n):
         coord = PolarCoords(diameter/2., (2.*np.pi*i/n)+rotation)
-        coords[i][0], coords[i][1] = coord.x+translate[0], coord.y+translate[1]
-    ax0.plot(np.transpose(np.concatenate((coords, coords),axis=0)[:n+1],
-                          (1,0))[0],np.transpose(np.concatenate((coords, coords),axis=0)[:n+1], (1,0))[1])
+        
+        coords[i][0] = coord.x + translate[0]
+        coords[i][1] = coord.y + translate[1]
+        
+    ax0.plot(
+        np.transpose(
+            np.concatenate((coords, coords), axis=0)[:n+1], (1,0)
+            )[0],
+        np.transpose(
+            np.concatenate((coords, coords), axis=0)[:n+1], (1,0)
+            )[1]
+        )
+
     elem_count = area/(x_spacing*y_spacing)
     
     if show_title == True:
-        ax0.set_title("Finite difference mesh demonstration\nDiameter = {:.2f} nm\n{} elements of size {}x{} nm"
-                      .format(diameter, elem_count, x_spacing, y_spacing))
+        ax0.set_title(
+            "Finite difference mesh demonstration\n \
+            Diameter = {:.2f} nm\n \
+            {} elements of size {}x{} nm".format(
+                diameter, elem_count, x_spacing, y_spacing
+                )
+            )
 
 
