adc2dd61261613d252aac2e479b75ebe81e51d32
Xander Marjoram <am31g12@soton.ac.uk>
2015-09-10 10:03:36 +0100

    Cleaning up code, calc.py.

--- polygonmeshtools/calc.py
+++ polygonmeshtools/calc.py
@@ -20,14 +20,19 @@ def in_poly(x, y, n, r=1, rotation=0, translate=(0,0), plot=False):
         coords = np.empty((n,2))
         for i in range(n):
             coord = PolarCoords(r, (2.*np.pi*i/n)+rotation)
-            coords[i][0], coords[i][1] = coord.x+translate[0], coord.y+translate[1]
+            coords[i][0] = coord.x + translate[0]
+            coords[i][1] = coord.y + translate[1]
         return coords
     
     def angle_test(x, y, r=r, n=n, rotation=rotation):
         """
-        Taking all pairs of adjacent vertices of the polygon, the sum of
-        the angles made by each pair of points about the point in question
-        should be approximately 2*pi.
+        Taking all pairs of adjacent vertices of the polygon, the sum
+        of the angles made by each pair of points about the point in
+        question should be approximately 2*pi.
+        
+        Method demonstrated at:
+        -----------------------
+        http://demonstrations.wolfram.com/IsAPointInsideOrOutsideARegularPolygon/
         """
         np.seterr(all='raise')
         
@@ -42,20 +47,34 @@ def in_poly(x, y, n, r=1, rotation=0, translate=(0,0), plot=False):
                 coord3 = coords[0]
                 
             #cosine rule to determine angle
-            length12 = math.sqrt((coord1[0]-coord2[0])**2 + (coord1[1]-coord2[1])**2)
-            length13 = math.sqrt((coord1[0]-coord3[0])**2 + (coord1[1]-coord3[1])**2)
-            length23 = math.sqrt((coord2[0]-coord3[0])**2 + (coord2[1]-coord3[1])**2)
+            length12 = math.sqrt((
+                coord1[0]-coord2[0])**2 + (coord1[1]-coord2[1])**2
+                )
+                                 
+            length13 = math.sqrt((
+                coord1[0]-coord3[0])**2 + (coord1[1]-coord3[1])**2
+                )
+                                 
+            length23 = math.sqrt((
+                coord2[0]-coord3[0])**2 + (coord2[1]-coord3[1])**2
+                )
+                                 
             if (np.isclose(length12,0)) or (np.isclose(length13,0)):
                 angle = 2*np.pi
                 break
 
             try:
-                angle_increment = (length12**2 + length13**2 - length23**2)/(2*length12*length13)
+                #cosine rule
+                angle_increment = (length12**2 + length13**2 - length23**2)
+                    / (2*length12*length13)
                 angle += np.arccos(angle_increment)
             except (FloatingPointError,ZeroDivisionError):
-                if np.isclose(angle_increment,-1): #inside, on the line
+                #inside, on the line
+                if np.isclose(angle_increment,-1):
                     return 2*np.pi #automatic pass
-                if np.isclose(angle_increment,1): #outside, 'on the line'
+                
+                #outside, 'on the line'
+                if np.isclose(angle_increment,1):
                     return 0 #automatic fail
         return angle
     
@@ -66,34 +85,49 @@ def in_poly(x, y, n, r=1, rotation=0, translate=(0,0), plot=False):
         ax0.set_xlim(-r+translate[0],r+translate[0])
         ax0.set_ylim(-r+translate[1],r+translate[1])
         ax0.set_aspect('equal')
-        ax0.plot(np.transpose(np.concatenate((coords, coords),axis=0)[:n+1],
-                              (1,0))[0],np.transpose(np.concatenate((coords, coords),axis=0)[:n+1], (1,0))[1])
+        
+        ax0.plot(
+            np.transpose(
+                np.concatenate(
+                    (coords, coords), axis=0)[:n+1], (1,0)
+                )[0],
+            np.transpose(
+                np.concatenate(
+                    (coords, coords), axis=0)[:n+1], (1,0)
+                )[1]
+            )
+                
         if np.isclose(2*np.pi, angle_test(x,y), atol=1e-4):
             dotcolour = 'g'
         else:
             dotcolour = 'r'
-        ax0.scatter([x,0+translate[0]],[y,0+translate[1]], c=[dotcolour,'b'], s=[20,10],lw = 0)
+            
+        ax0.scatter(
+            [x,0+translate[0]], [y,0+translate[1]],
+            c = [dotcolour,'b'], s = [20,10], lw = 0
+            )
+            
     return np.isclose(2*np.pi, angle_test(x,y), atol=1e-4)
-
-
-
-
 
 
 def find_circumradius(n, side = 0, apothem = 0):
     """
-    Returns the radius of the circumscribing circle for a regular polygon
-    given the side length or the apothem (distance from circumcentre to 
-    centre of a face).
+    Returns the radius of the circumscribing circle for a regular
+    polygon given the side length or the apothem (distance from
+    circumcentre to centre of a face).
     """
     if side == 0 and apothem != 0:
         circumradius = apothem/(np.cos(np.pi/n))
     elif side != 0 and apothem == 0:
         circumradius = side/(2*np.sin(np.pi/n))
     elif side == 0 and apothem == 0:
-        raise ValueError("Please specify a side length or an apothem.")
+        raise ValueError(
+            "Please specify a side length or an apothem."
+            )
     else:
-        raise ValueError("Please specify side length OR apothem, not both!")
+        raise ValueError(
+            "Please specify side length OR apothem, not both!"
+            )
     return circumradius
 
 
