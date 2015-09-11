6ef6dc194c13dbdbc5ede9afb584518c74985f16
Xander Marjoram <am31g12@soton.ac.uk>
2015-09-11 16:47:29 +0100

    Incremented version number to 0.43.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 setup(
     name = "polygonmeshtools",
     packages = ["polygonmeshtools"],
-    version = "0.42",
+    version = "0.43",
     description = "A package for creating polygon-shaped geometries \
         from cuboidal finite difference elements",
     author = "Xander Marjoram",
